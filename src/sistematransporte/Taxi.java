5f21c21889b32e5d8c5713cf99599b7c0fadb33a
melissaZarate <meli.za1009@gmail.com>
2026-04-13 11:25:21 -0400

    creamos clase taxi

--- src/sistematransporte/Taxi.java
+++ src/sistematransporte/Taxi.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package sistematransporte;
 
 /**
  *
  * @author HP
  */
-public class Taxi {
+public class Taxi extends Transporte{
+
+    public Taxi(String nombre) {
+        super(nombre);
+    }
+      //clases abstractas
+    @Override
+    public String moverse(){
+        String seMueve="se mueve con motor";
+        return seMueve;
+    }
+    @Override
+    public String capacidad(){
+        
+        return "Tiene capacidad para 4 personas";
+    }
+    @Override
+    public boolean tieneCombustible(){
+        return true;
+    }
+    public String tipoCombustible(){
+        return "Gasolina";
+    }
     
 }
