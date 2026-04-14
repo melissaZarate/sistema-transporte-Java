6850dbfea7a6e4b88dfa832ca664de2cb389ce75
melissaZarate <meli.za1009@gmail.com>
2026-04-14 05:43:11 -0400

    guardando cambios  despues de pull

--- src/sistematransporte/SistemaTransporte.java
+++ src/sistematransporte/SistemaTransporte.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package sistematransporte;
 
-/**
- *
- * @author HP
- */
 public class SistemaTransporte {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Sistema con abstraccion+ herencoa+polifpormismo
+        //aqui lo llamamos con un lista dinamica
     }
     
 }
